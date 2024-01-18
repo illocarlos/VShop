9d39a63f2b268401b539eed59b79a47d8bac7494
illocarlos <carlosalbendiz93@gmail.com>
2024-01-18 17:34:09 +0100

    install dependence and tailwind

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import IndexShopView from '@/views/IndexShopView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: IndexShopView
+    },
 
   ]
 })
---
+++ src/views/IndexShopView.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>INDEX SHOP    </h1>
+</template>
