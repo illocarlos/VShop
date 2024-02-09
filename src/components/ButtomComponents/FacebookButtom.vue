30877adfb20cbcf6ba6c4e6aac0ab01e6633cc76
illocarlos <carlosalbendiz93@gmail.com>
2024-02-09 23:41:55 +0100

    foother

--- src/components/ButtomComponents/FacebookButtom.vue
+++ src/components/ButtomComponents/FacebookButtom.vue
@@ -2,17 +2,43 @@
 </script>
 
 <template>
-  <section class="flex justify-center items-center">
-    <button
-      class="w-7 h-7 group flex justify-center p-3 rounded-md drop-shadow-xl from-gray-800 bg-[#316FF6] text-white font-semibold hover:translate-y-3 hover:rounded-[50%] transition-all duration-500 hover:from-[#331029] hover:to-[#310413]">
-      <span class="facebookSVG absolute top-1">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="svgIcon h-5" fill="currentColor">
-          <path
-            d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z">
-          </path>
-        </svg>
-      </span>
-      <span
-        class="absolute opacity-0 group-hover:opacity-100 group-hover:text-white group-hover:text-xs group-hover:-translate-y-8 duration-700">Facebook</span>
-    </button>
-</section></template>
+    <div class="svg-container">
+     <a href="https://www.facebook.com" target="_blank" class="flex items-center space-x-3 hover:text-sky-400 transition">
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="white" class="w-5" viewBox="0 0 16 16">
+  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                                  </svg>
+                                  <span class="hidden md:block " >Facebook</span>
+                              </a>
+                                </div>
+    </template>
+<style scoped>
+/* Estilo por defecto */
+.svg-container svg path {
+  fill: white;
+
+  /* Color por defecto */
+  transition: fill 0.3s;
+  /* Transición suave */
+}
+
+.svg-container span {
+  color: white;
+
+  /* Color por defecto */
+  transition: color 0.3s;
+  /* Transición suave */
+}
+
+/* Estilo al hacer hover */
+.svg-container:hover span {
+
+  color: rgb(109, 104, 252);
+  /* Color al hacer hover */
+}
+
+.svg-container:hover svg path {
+  fill: rgb(109, 104, 252);
+
+  /* Color al hacer hover */
+}
+</style>
