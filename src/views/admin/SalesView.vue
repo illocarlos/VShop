eed88346a536b5636edc839132a6b470c51a024e
illocarlos <carlosalbendiz93@gmail.com>
2024-02-13 22:55:41 +0100

    sales rework

--- src/views/admin/SalesView.vue
+++ src/views/admin/SalesView.vue
@@ -14,20 +14,20 @@ const formatter = ref({
 </script>
 
 <template>
-  <h1 class="text-4xl font-black my-10">Resumen de Ventas</h1>
+  <h1 class="text-4xl font-black my-10">Sumary of sales</h1>
 
   <div class=" md:flex md:items-start gap-5">
     <div class="md:w-/2 lg:w-1/3 bg-white flex justify-center p-5">
-      <VueTailwindDatePicker i18n="es-mx" as-single no-input v-model="sales.date" :formatter="formatter" />
+      <VueTailwindDatePicker i18n="es-US" as-single no-input v-model="sales.date" :formatter="formatter" />
     </div>
     <div class="md:w-1/2 lg:w-2/3 space-y-5 lg:h-screen lg:overflow-y-scroll p-5 pb-32">
       <p v-if="sales.isDateSelected" class="text-center text-lg">
-        Ventas de la fecha: <span class="font-black">{{ sales.date }}</span>
+        sales of the date: <span class="font-black">{{ sales.date }}</span>
       </p>
-      <p v-else class="text-center text-lg">Selecciona una fecha</p>
+      <p v-else class="text-center text-lg">select a date</p>
 
       <div v-if="sales.salesCollection.length" class="space-y-5">
-        <p class="text-right text-2xl">Total del día:
+        <p class="text-right text-2xl">Total of day:
           <span class="font-black">
             {{ formatCurrency(sales.totalSalesOfDay) }}
           </span>
@@ -36,7 +36,7 @@ const formatter = ref({
         <SaleDetails v-for="sale in sales.salesCollection" :key="sale.id" :sale="sale" />
       </div>
       <p v-else-if="sales.noSales" class="text-lg text-center">
-        No hay ventas en este día
+   No sales
       </p>
     </div>
   </div>
